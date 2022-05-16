e3dcbe8a1bb8689331de0888ddc6444568890498
camiloJosue222 <camilojosue222@gmail.com>
2022-05-15 22:13:27 -0500

    Agregar archivos de proyecto.

---
+++ MVCCrud/Models/ViewModels/ListTablaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCCrud.Models.ViewModels
+{
+    public class ListTablaViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+       /* public DateTime Fecha_nacimiento { get; set; }*/ 
+
+    }
+}
---
+++ MVCCrud/Models/ViewModels/TablaViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCCrud.Models.ViewModels
+{
+    public class TablaViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(10)]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(10)]
+        [Display(Name = "Correo")]
+        public string Correo { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de nacimiento")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Fecha_nacimiento { get; set; }
+    }
+}
